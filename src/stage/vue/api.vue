1ea7b6217720529bf69f79ce6cd47af30ca3cf12
vikoumo <dwy.css@gmail.com>
2018-12-05 11:26:12 +0800

    apivue

--- src/stage/vue/api.vue
+++ src/stage/vue/api.vue
@@ -1,13 +1,60 @@
 <template>
-  <div id='vueRouterPage1'>
-    <el-button type="text" @click='jumpToPage'>jumpToPage2</el-button>
+  <div id='vueApi'>
+    <h3>原始 HTML</h3>
+    <p>Using mustaches: {{ rawHtml }}</p>
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+    <h3>v-if</h3>
+    <p v-if="seen">现在你看到我了</p>
+    <h3>computed vs methods</h3>
+    <p>Original message: "{{ message }}"</p>
+    <p>Computed reversed message: "{{ computedReversedMessage }}"</p>
+    <p>Methods reversed message: "{{ methodsReversedMessage() }}"</p>
+    <h3>computed vs watch</h3>
+    <p>computed fullName {{ computedFullName }}</p>
+    <p>watch fullName {{ fullName }}</p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#vueApi {
+  color: #2c3e50;
+  font-size: 16px;
+  h3 {
+    font-size: 20px;
+  }
+}
+</style>
+
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      rawHtml: '<span style="color:red">this should be red</span>',
+      seen: true,
+      message: 'hello',
+      firstName: 'Foo',
+      lastName: 'Bar',
+      fullName: 'Foo Bar'
+    };
+  },
+  computed: {
+    // 计算属性的 getter
+    // 计算属性是基于它们的依赖进行缓存的
+    computedReversedMessage() {
+      return this.message.split('').reverse().join('');
+    },
+    computedFullName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  },
+  watch: {
+    firstName(val) {
+      this.fullName = `${val} ${this.lastName}`;
+    },
+    lastName(val) {
+      this.fullName = `${this.firstName} ${val}`;
+    }
   },
   methods: {
     jumpToPage() {
@@ -16,6 +63,9 @@ export default {
         params: { id: 123 },
         query: { plan: 'private', q: 'vue' }
       });
+    },
+    methodsReversedMessage() {
+      return this.message.split('').reverse().join('');
     }
   }
 };
